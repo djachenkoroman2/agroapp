9f5079091ca2c5f544346ec8e97a12d9fbe45462
djachenkoroman2 <djachenko.roman2@gmail.com>
2025-10-22 18:52:14 +0300

    Add config.yaml

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def main():
-    print("Hello from agroapp!")
+import hydra
+from omegaconf import DictConfig
+
+@hydra.main(config_path=".", config_name="config", version_base=None)
+def main(cfg: DictConfig):
+    print(f"Image FileName: {cfg.img_filename}")
 
 
 if __name__ == "__main__":
